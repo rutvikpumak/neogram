c0999b0660bea12979ebd446a8b1035a0961f78f
Rutvik Umak <rutvikumak@gmail.com>
2022-05-14 13:01:20 +0530

    fix : styles for auth forms

--- src/features/Auth/Login.jsx
+++ src/features/Auth/Login.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/social-logo.png";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "./authSlice";
-import { toast } from "react-toastify";
-
+import "./Auth.css";
 export function Login() {
   const { token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -33,7 +32,7 @@ export function Login() {
 
   useEffect(() => token && navigate("/"), [token]);
   return (
-    <div className="w-full h-screen text-gray-800  px-4 py-6 flex flex-col justify-center sm:py-12">
+    <div className="auth-container w-full h-screen text-gray-800  px-4 py-6 flex flex-col justify-center sm:py-12 sm:h-full">
       <div className="w-2/5 py-3 sm:max-w-xl mx-auto text-center sm:w-11/12">
         <div className="mt-4 bg-white shadow-md sm:rounded-lg text-left">
           <div className="h-2 bg-blue-500 rounded-t-md"></div>
@@ -41,8 +40,8 @@ export function Login() {
             <img className="h-12" src={logo} />
             <p className="text-blue-400 title text-3xl ml-2 font-bold">Welcome to Neogram</p>
           </div>
-          <div className="px-12 py-4 pb-8">
-            <span className="text-2xl flex justify-center font-semibold border-b-2 border-blue-400 pb-2">
+          <div className="px-12 py-4 pb-8 sm:px-6">
+            <span className="text-2xl flex justify-center font-semibold border-b-2 border-blue-400 pb-2 sm:text-xl">
               Sign In
             </span>
             <div className="pt-8">
@@ -50,7 +49,7 @@ export function Login() {
               <input
                 type="text"
                 placeholder="Email"
-                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md"
+                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md sm:py-4"
                 value={loginForm.email}
                 onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
                 required
@@ -59,14 +58,14 @@ export function Login() {
               <input
                 type="password"
                 placeholder="Password"
-                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md"
+                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md sm:py-4 sm:mt-1"
                 value={loginForm.password}
                 onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
                 required
               />
               <div>
                 <button
-                  className="font-semibold w-full my-6 bg-blue-400 text-white py-2 px-6 rounded-lg hover:bg-blue-500"
+                  className="font-semibold w-full my-6 bg-blue-400 text-white py-2 px-6 rounded-lg hover:bg-blue-500 sm:py-1"
                   onClick={() => loginHandler()}
                 >
                   Sign In With Test
--- src/features/Auth/SignUp.jsx
+++ src/features/Auth/SignUp.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/social-logo.png";
 import { useDispatch, useSelector } from "react-redux";
 import { signUpUser } from "./authSlice";
-
+import "./Auth.css";
 export function SignUp() {
   const { token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -33,7 +33,7 @@ export function SignUp() {
   };
 
   return (
-    <div className="w-full h-screen text-gray-800  px-4 py-6 flex flex-col justify-center sm:py-12">
+    <div className="auth-container w-full h-screen text-gray-800  px-4 py-6 flex flex-col justify-center sm:py-12">
       <div className="w-2/5 py-3 sm:max-w-xl mx-auto text-center sm:w-11/12">
         <div className="mt-4 bg-white shadow-md sm:rounded-lg text-left">
           <div className="h-2 bg-blue-500 rounded-t-md"></div>
@@ -41,18 +41,18 @@ export function SignUp() {
             <img className="h-12" src={logo} />
             <p className="text-blue-400 title text-3xl ml-2 font-bold">Welcome to Neogram</p>
           </div>
-          <div className="px-12 py-4 pb-8 ">
-            <span className="text-2xl flex justify-center font-semibold border-b-2 border-blue-400 pb-2">
+          <div className="px-12 py-4 pb-8 sm:px-6 ">
+            <span className="text-2xl flex justify-center font-semibold border-b-2 border-blue-400 pb-2 sm:text-xl">
               Sign Up
             </span>
             <div className="pt-8">
-              <div className="flex gap-4 sm:flex-col mb-4">
+              <div className="flex gap-4 sm:flex-col mb-4 sm:gap-2 sm:mb-2">
                 <div>
                   <label className="block font-semibold">First Name</label>
                   <input
                     type="text"
                     placeholder="First Name"
-                    className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md"
+                    className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md sm:py-4"
                     value={signUpForm.firstName}
                     onChange={(e) => fillFormValue(e, "firstName")}
                     required
@@ -63,7 +63,7 @@ export function SignUp() {
                   <input
                     type="text"
                     placeholder="Last Name"
-                    className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md"
+                    className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md sm:py-4"
                     value={signUpForm.lastName}
                     onChange={(e) => fillFormValue(e, "lastName")}
                     required
@@ -74,23 +74,23 @@ export function SignUp() {
               <input
                 type="text"
                 placeholder="Email"
-                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md"
+                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md sm:py-4"
                 value={signUpForm.email}
                 onChange={(e) => fillFormValue(e, "email")}
                 required
               />
-              <label className="block mt-3 font-semibold">Password</label>
+              <label className="block mt-3 font-semibold sm:mt-2">Password</label>
               <input
                 type="password"
                 placeholder="Password"
-                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md"
+                className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-md sm:py-4"
                 value={signUpForm.password}
                 onChange={(e) => fillFormValue(e, "password")}
                 required
               />
               <div>
                 <button
-                  className="font-semibold w-full my-6 bg-blue-400 text-white py-2 px-6 rounded-lg hover:bg-blue-500"
+                  className="font-semibold w-full my-6 bg-blue-400 text-white py-2 px-6 rounded-lg hover:bg-blue-500 sm:py-1"
                   onClick={() => signUpHandler()}
                 >
                   Create Account
